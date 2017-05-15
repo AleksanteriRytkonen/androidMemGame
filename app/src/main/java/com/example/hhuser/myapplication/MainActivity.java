3c1c62157ca040897a709aad21d87e715b487bb0
hhuser <hhuser>
2017-05-15 17:48:27 +0300

    second commit

--- app/src/main/java/com/example/hhuser/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hhuser/myapplication/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.hhuser.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        i1 = (ImageView) this.findViewById(R.id.i1);
+        i2 = (ImageView) this.findViewById(R.id.i2);
+        i3 = (ImageView) this.findViewById(R.id.i3);
+        i4 = (ImageView) this.findViewById(R.id.i4);
+        i5 = (ImageView) this.findViewById(R.id.i5);
+        i6 = (ImageView) this.findViewById(R.id.i6);
+        i7 = (ImageView) this.findViewById(R.id.i7);
+        i8 = (ImageView) this.findViewById(R.id.i8);
+        i9 = (ImageView) this.findViewById(R.id.i9);
+        i10 = (ImageView) this.findViewById(R.id.i10);
+        i11 = (ImageView) this.findViewById(R.id.i11);
+        i12 = (ImageView) this.findViewById(R.id.i12);
     }
 }
